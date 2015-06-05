d58db59316d3b72a55b7284ed7cb6e9b064f7af2
longqi <longqi90@gmail.com>
2015-06-05 23:37:25 +0800

    prototype finished

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 # fig.set_size_inches(10.5, 10.5)
-ax.set_xlim(-4, 4)
-ax.set_ylim(-4, 4)
-time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+big_radius = 4
+small_radius = big_radius / 2
+ax.set_xlim(-big_radius, big_radius)
+ax.set_ylim(-big_radius, big_radius)
 
-x0 = np.arange(-4, 4, 0.01)  # x0-array for big  cycle
+x0 = np.arange(-big_radius, big_radius, 0.01)  # x0-array for big  cycle
 
 # draw the big cycle
-cycle0_line0, cycle0_line1 = ax.plot(x0, np.sqrt(16 - x0 * x0), x0, 0 - np.sqrt(16 - x0 * x0))
+cycle0_line0, cycle0_line1 = ax.plot(x0, np.sqrt(big_radius * big_radius - x0 * x0), x0, 0 - np.sqrt(16 - x0 * x0))
 plt.setp(cycle0_line0, color='r')
 
 # draw two lines first
@@ -24,25 +25,61 @@ line0 = ax.plot(x0, x0 - x0)
 line1 = ax.plot(x0 - x0, x0)
 
 # the angle we will use
-a0 = np.arange(0, 4 * np.pi, 0.01)  # a-array for small  cycle
+a0 = np.arange(0, 2 * np.pi, 0.1)  # a-array for small  cycle
 
-dot0, = ax.plot(a0 - a0 + 1, a0)
+dot0_angle = 0
+dot1_angle = np.pi * 1 / 6
+dot2_angle = np.pi * 2 / 6
+dot3_angle = np.pi * 3 / 6
+dot4_angle = np.pi * 4 / 6
+dot5_angle = np.pi * 5 / 6
+dot6_angle = np.pi * 6 / 6
 
 
-def animate(i):
-    dot0.set_ydata(i)  # update the data
 
-    return dot0,
+
+# t = np.range
+dot0, dot1, dot2, dot3, dot4, dot5, dot6 = ax.plot(0, 0, 'or',
+                                                   0, 0, 'or',
+                                                   0, 0, 'or',
+                                                   0, 0, 'or',
+                                                   0, 0, 'or',
+                                                   0, 0, 'or',
+                                                   0, 0, 'or')
+
+
+def animate(epsilon):
+    dot0.set_xdata(2 * small_radius * np.cos(epsilon - dot0_angle) * np.cos(dot0_angle))  # update the data
+    dot0.set_ydata(2 * small_radius * np.cos(epsilon - dot0_angle) * np.sin(dot0_angle))  # update the data
+
+    dot1.set_xdata(2 * small_radius * np.cos(epsilon - dot1_angle) * np.cos(dot1_angle))  # update the data
+    dot1.set_ydata(2 * small_radius * np.cos(epsilon - dot1_angle) * np.sin(dot1_angle))  # update the data
+
+    dot2.set_xdata(2 * small_radius * np.cos(epsilon - dot2_angle) * np.cos(dot2_angle))  # update the data
+    dot2.set_ydata(2 * small_radius * np.cos(epsilon - dot2_angle) * np.sin(dot2_angle))  # update the data
+
+    dot3.set_xdata(2 * small_radius * np.cos(epsilon - dot3_angle) * np.cos(dot3_angle))  # update the data
+    dot3.set_ydata(2 * small_radius * np.cos(epsilon - dot3_angle) * np.sin(dot3_angle))  # update the data
+
+    dot4.set_xdata(2 * small_radius * np.cos(epsilon - dot4_angle) * np.cos(dot4_angle))  # update the data
+    dot4.set_ydata(2 * small_radius * np.cos(epsilon - dot4_angle) * np.sin(dot4_angle))  # update the data
+
+    dot5.set_xdata(2 * small_radius * np.cos(epsilon - dot5_angle) * np.cos(dot5_angle))  # update the data
+    dot5.set_ydata(2 * small_radius * np.cos(epsilon - dot5_angle) * np.sin(dot5_angle))  # update the data
+
+    dot6.set_xdata(2 * small_radius * np.cos(epsilon - dot6_angle) * np.cos(dot6_angle))  # update the data
+    dot6.set_ydata(2 * small_radius * np.cos(epsilon - dot6_angle) * np.sin(dot6_angle))  # update the data
+
+    return dot0, dot1, dot2, dot3, dot4, dot5, dot6
 
 
 # Init only required for blitting to give a clean slate.
 def init():
-    dot0.set_ydata(np.ma.array(a0, mask=True))
-    return dot0,
+    return dot0, dot1, dot2, dot3, dot4, dot5, dot6
 
 
-ani = animation.FuncAnimation(fig, animate, np.arange(0, np.pi, 0.1), init_func=init,
-                              interval=25, blit=False)
+ani = animation.FuncAnimation(fig, animate, np.arange(0, 2 * np.pi, 0.1), init_func=init,
+                              interval=20, blit=False)
 
 fig.show()
 plt.show()
--- simple_anim.py
+++ simple_anim.py
@@ -7,22 +7,22 @@ import matplotlib.animation as animation
 
 fig, ax = plt.subplots()
 
-x = np.arange(0, 2 * np.pi, 0.01)  # x0-array
+x = np.arange(0, 2 * np.pi, 0.1)  # x0-array
 line, = ax.plot(x, np.sin(x))
 
 
 def animate(i):
-    line.set_ydata(np.sin(x + i / 10.0))  # update the data
+    line.set_ydata(np.sin(x + i))  # update the data
+    print(line.get_ydata())
     return line,
 
 
 # Init only required for blitting to give a clean slate.
 def init():
-
     line.set_ydata(np.ma.array(x, mask=True))
     return line,
 
 
-ani = animation.FuncAnimation(fig, animate, np.arange(1, 200), init_func=init,
+ani = animation.FuncAnimation(fig, animate, np.arange(1, 20, 0.1), init_func=init,
                               interval=25, blit=False)
 plt.show()
